5473a9e7d553c21d7f95ba08de1668dcf6dac890
Daniel <daniel.urribarri@powercoders.org>
2023-10-27 13:28:05 +0200

    index and js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,16 @@
-// Define the questions array as an array of objects.
-/* const questions = [
-    {
-        question: "What does JSON stand for?",
-        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
-        correct: 0
-    },
-    {
-        question: "1 What does JSON stand for?",
-        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
-        correct: 0
-    },
-    {
-        question: "2 What does JSON stand for?",
-        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
-        correct: 0
-    },
-    {
-        question: "3 What does JSON stand for?",
-        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
-        correct: 0
-    }
-]; */
-
-// Remove this line as it's not needed.
-// const json = JSON.stringify(questions);
-
-// Import 'questions.json' and store it in the 'questions' variable.
-/* import { questions } from 'questions.json';
- */
-// Perform the fetch request to get questions from an API.
-
-
 
 fetch("csvjson.json")
     .then(response => response.json())
     .then(json => {
-        const mainTag = document.querySelector("main");
+        const ulTag = document.querySelector("ul");
         const companies = json;
-        console.log(companies);
-        companies.forEach(company => {
-            console.log(company);
-            const h2Tag = document.createElement("h2");
-            h2Tag.innerHTML = company.CompanyName_23-2; // Check the property name
-            mainTag.appendChild(h2Tag);
+        companies.forEach((company, index) => {
+            const liTag = document.createElement("li");
+            liTag.id = `company${index}`;
+            liTag.innerHTML = company.CompanyName;
+            ulTag.appendChild(liTag);
+            
+            const ImgTag = document.createElement("img");
         });
     })
     .catch(error => {
