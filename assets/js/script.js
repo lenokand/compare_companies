cdd7cbbe4290d490b3f5fe45ce20151f00fdfd07
Chaewon <chaewon.yoo@powers.org>
2023-10-27 10:17:15 +0200

    update js file

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+// Define the questions array as an array of objects.
+const questions = [
+    {
+        question: "What does JSON stand for?",
+        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
+        correct: 0
+    },
+    {
+        question: "1 What does JSON stand for?",
+        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
+        correct: 0
+    },
+    {
+        question: "2 What does JSON stand for?",
+        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
+        correct: 0
+    },
+    {
+        question: "3 What does JSON stand for?",
+        answers: ["JavaScript Object Notation", "Java Soon Objects Notification", "JavaScript Objects Notation", "JavaScript Object Notification"],
+        correct: 0
+    }
+];
+
+// Remove this line as it's not needed.
+// const json = JSON.stringify(questions);
+
+// Import 'questions.json' and store it in the 'questions' variable.
+import { questions } from 'questions.json';
+
+// Perform the fetch request to get questions from an API.
+fetch("https://opentdb.com/api.php?amount=10&category=18")
+    .then(response => response.json())
+    .then(json => {
+        const mainTag = document.querySelector("main");
+        const questions = json.results;
+
+        questions.forEach((questionObj, index) => {
+            const h2Tag = document.createElement("h2");
+            // Use the current question from 'questionObj' instead of 'question[0]'.
+            h2Tag.innerHTML = questionObj.question;
+            mainTag.appendChild(h2Tag);
+
+            // Create a ulTag to hold answer options.
+            const ulTag = document.createElement("ul");
+
+            // Iterate through incorrect answers and create radio buttons.
+            questionObj.incorrect_answers.forEach((answer, index) => {
+                const liTag = document.createElement("li");
+                ulTag.appendChild(liTag);
+
+                const inputTag = document.createElement("input");
+                inputTag.id = "answer" + index;
+                inputTag.name = "answer";
+                inputTag.type = "radio";
+                inputTag.value = answer;
+                liTag.appendChild(inputTag);
+
+                const labelTag = document.createElement("label");
+                labelTag.setAttribute("for", "answer" + index);
+                labelTag.innerText = answer;
+                liTag.appendChild(labelTag);
+            });
+
+            // Append the ulTag to the mainTag.
+            mainTag.appendChild(ulTag);
+        });
+    })
+    .catch(error => {
+        console.error(error);
+    });
